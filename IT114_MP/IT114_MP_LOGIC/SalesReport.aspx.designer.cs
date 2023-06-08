d80d5105cb9c89c5b2794ab7348017f2f8f82a9b
jelliness <jellyannekaye0504@gmail.com>
2023-06-09 00:34:39 +0800

    SALES PRODUCT UI
    AAAAAAAAAAAAAAA

--- IT114_MP/IT114_MP_LOGIC/SalesReport.aspx.designer.cs
+++ IT114_MP/IT114_MP_LOGIC/SalesReport.aspx.designer.cs
@@ -24,6 +24,51 @@ namespace IT114_MP_LOGIC
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// MyTableDaily control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table MyTableDaily;
+
+        /// <summary>
+        /// Label7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label7;
+
+        /// <summary>
+        /// Label11 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label11;
+
+        /// <summary>
         /// ddlMonths control.
         /// </summary>
         /// <remarks>
@@ -42,13 +87,13 @@ namespace IT114_MP_LOGIC
         protected global::System.Web.UI.WebControls.Table MyTableMonthly;
 
         /// <summary>
-        /// MyTableDaily control.
+        /// Label5 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Table MyTableDaily;
+        protected global::System.Web.UI.WebControls.Label Label5;
 
         /// <summary>
         /// MyTableOverall control.
