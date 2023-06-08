ef49e449d13a898c0728aae36116f1445622bc8a
Kane Justine A. Cometa <kane55justine@gmail.com>
2023-06-08 23:56:33 +0800

    Modify Nav Bar - Transaction, Admin, Report Page

--- IT114_MP/IT114_MP_LOGIC/AdminPage.aspx.designer.cs
+++ IT114_MP/IT114_MP_LOGIC/AdminPage.aspx.designer.cs
@@ -15,13 +15,31 @@ namespace IT114_MP_LOGIC
     {
 
         /// <summary>
-        /// btnLogout control.
+        /// Label2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnLogout;
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// LinkButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+
+        /// <summary>
+        /// LinkButton2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton2;
 
         /// <summary>
         /// lblUname control.
